a1870fd84e5d7e853069fe8012df59a5d05b512f
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-21 22:45:56 +0700

    Create static method of Book model

--- src/models/book.model.js
+++ src/models/book.model.js
@@ -1,21 +1,29 @@
 const { Schema, model } = require('mongoose');
 
 const BookSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    genre: {
-        type: String,
-        required: true
-    },
-    authorId: {
-        type: Schema.Types.ObjectId,
-        ref: 'authors',
-        required: true
-    }
+	name: {
+		type: String,
+		required: true
+	},
+	genre: {
+		type: String,
+		required: true
+	},
+	authorId: {
+		type: Schema.Types.ObjectId,
+		ref: 'authors',
+		required: true
+	}
 }, {
-    timestamps: true
+	timestamps: true
 });
+
+BookSchema.statics.getBooksOfAuthor = function (authorId) {
+	return this.find({ authorId });
+};
+
+BookSchema.statics.getAllBooks = function () {
+	return this.find();
+}
 
 module.exports = model('books', BookSchema);
--- src/resolver/resolver.js
+++ src/resolver/resolver.js
@@ -2,43 +2,43 @@ const Author = require('../models/author.model');
 const Book = require('../models/book.model');
 
 const resolvers = {
-    Query: {
-        book: async (parent, args) => {
-            return Book.findById(args.id);
-        },
-        books: async () => {
-            return Book.find();
-        },
-        author: async (parent, args) => {
-            return Author.findById(args.id);
-        },
-        authors: async () => {
-            return Author.find();
-        },
-    },
+	Query: {
+		book: async (parent, args) => {
+			return Book.findById(args.id);
+		},
+		books: async () => {
+			return Book.getAllBooks();
+		},
+		author: async (parent, args) => {
+			return Author.findById(args.id);
+		},
+		authors: async () => {
+			return Author.find();
+		},
+	},
 
-    Author: {
-        books: async (parent, args) => {
-            return Book.find({ authorId: parent.id });
-        }
-    },
+	Author: {
+		books: async (parent, args) => {
+			return Book.getBooksOfAuthor(parent.id);
+		}
+	},
 
-    Book: {
-        author: async (parent, args) => {
-            return Author.findById(parent.authorId);
-        }
-    },
+	Book: {
+		author: async (parent, args) => {
+			return Author.findById(parent.authorId);
+		}
+	},
 
-    Mutation: {
-        createAuthor: async (parent, args) => {
-            const newAuthor = new Author(args);
-            return newAuthor.save();
-        },
-        createBook: async (parent, args) => {
-            const newBook = new Book(args)
-            return newBook.save();
-        }
-    }
+	Mutation: {
+		createAuthor: async (parent, args) => {
+			const newAuthor = new Author(args);
+			return newAuthor.save();
+		},
+		createBook: async (parent, args) => {
+			const newBook = new Book(args)
+			return newBook.save();
+		}
+	}
 };
 
 module.exports = resolvers;
