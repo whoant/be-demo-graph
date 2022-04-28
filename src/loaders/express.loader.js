f0e13e196067e9e0f087aec0decaca81b1f0ff01
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-29 01:09:49 +0700

    Add morgan

--- src/loaders/express.loader.js
+++ src/loaders/express.loader.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
+const routes = require('../api/routes');
+const AppError = require("../api/helpers/appError.helper");
+const { handleError } = require("../api/helpers/response.helper");
+
+
 module.exports = expressApp => {
+    expressApp.use(cors())
     expressApp.use(express.json());
     expressApp.use(morgan('dev'));
+    routes(expressApp);
+    // expressApp.all('*', (req, res, next) => {
+    //     next(new AppError(`Can't find ${req.originalUrl} on this server !`, 400));
+    // });
+    expressApp.use(handleError);
 };
